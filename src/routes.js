289755d86efe2bde602391a282c797729457f25d
Nicollas <nicollasmk9@gmail.com>
2025-10-05 15:48:44 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const { Router } = new require("express");
+const routes = new Router();
+
+routes.get("/", (req, res) => {
+    return res.json({message: "Hello"});
+});
+
+module.exports = routes; 
